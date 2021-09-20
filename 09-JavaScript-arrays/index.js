9378f39e7827fb2c50592213fcadadea93ead8ff
mierjar <65968973+mierjar@users.noreply.github.com>
2021-09-20 13:26:22 +0200

    vi har arbejdet med arrays

--- 09-JavaScript-arrays/index.js
+++ 09-JavaScript-arrays/index.js
@@ -1,7 +1,35 @@
+let main = document.querySelector('main')
+let input = document.querySelector('#input')
 
 
-const sayHi = name => console.log('hi ' + name)
+const addCard = name =>{
+let newCard = document.createElement('div')
+newCard.classList.add('card')
 
-sayHi('viktor')
+let newH = document.createElement('h1')
+newH.innerHTML = name
+newCard.append(newH)
+main.append(newCard)
+}
 
-document.querySelector('body').addEventListener('click', e=>console.log(e.target))
+addCard('daddy')
+
+//vi vil gerne lave et array med tekst vaiabler
+let names = ['mie', 'theis', 'andreas', 'bjørn', 'bjarke', 'sofus', 'marius', 'viktor', 'nagib', 'emil', 'simon', 'frederik']
+
+names.push('mommy')
+
+
+names.map( name => addCard(name) )
+
+input.addEventListener('input', () => {
+    let results= names.filter( name => name.toLowerCase().includes(input.value.toLowerCase()))
+    main.innerHTML = ' '
+    results.map(name => addCard(name))
+})
+
+
+//her er et loop
+//for(let i = 0; i < names.length; i++){
+//    console.log (names[i])
+//}
