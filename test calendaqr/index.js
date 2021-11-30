fc6074662b535355e3d0658fb665e4c95588aa93
mierjar <65968973+mierjar@users.noreply.github.com>
2021-11-30 13:35:41 +0100

    Co-authored-by: Albatrossdk <Albatrossdk@users.noreply.github.com>

--- test calendaqr/index.js
+++ test calendaqr/index.js
@@ -1,17 +1,4 @@
+console.log("Mie er Bjørns Daddy/Mommy/Parent")
+console.log("Mie har god røv")
+console.log("Mie should buy a strap on 🥵")
 
-let api_key = 'PQfbUsBdhN1myEfLa6CWzwL9Z1s9Tc3g'
-
-fetch('https://api.giphy.com/v1/gifs/trending?api_key=' + api_key + '&limit=26&rating=pg-13')
-    .then( Response => Response.json() )
-    .then( json => {
-        json.data.map(image => insertImage(image.images.fixed_width.url))
-    })
-    
-    
-    
-    
-    const insertImage = (url) => {
-        let img = document.createElement('img')
-        img.src = url
-        document.querySelector('.container').append(img)
-    }
