f294038ed00526cfc144b5b5160a708af6b733de
mierjar <65968973+mierjar@users.noreply.github.com>
2022-01-12 10:10:33 +0100

    hej

---
+++ 113-p5-BOLD/index.js
@@ -0,0 +1,71 @@
+let x, y, diameter
+let gravity = 1
+let friction = 0.97
+let velocity = 0
+let updrift = 25
+let rectX, rectY, rectW, rectH
+let rectSpeed = 5
+let score = 0
+
+function showRect() {
+    rect(rectX, rectY, rectW, rectH)
+    rect(rectX, 0, rectW, rectH)
+}
+
+function updateRect() {
+    rectX -= rectSpeed
+    if (rectX <=0){
+        rectX = windowWidth
+        rectH = random(100,300)
+        rectY = windowHeight - rectH
+    }
+    if (rectX == windowWidth/2){
+        score++
+    }
+}
+
+
+
+function setup(){
+    createCanvas(windowWidth, windowHeight)
+    background('pink')
+    x = windowWidth/2
+    diameter = 32
+    y = diameter/2
+    rectW = 40
+    rectH = 100
+    rectX = windowWidth
+    rectY = windowHeight - rectH
+    select('#info').html(score)
+}
+
+function show() {
+    ellipse(x, y, diameter)
+}
+
+function update() {
+    velocity += gravity
+    velocity *= friction
+    y += velocity
+    if(y > windowHeight - diameter/2){
+        y = windowHeight - diameter/2
+        velocity = -velocity
+    }
+    select('#info').html(velocity)
+}
+
+function draw() {
+    background('pink')
+    show()
+    update()
+    showRect()
+    updateRect()
+    select('#info').html(score)
+}
+
+function keyPressed(key){
+    console.log(key)
+    if(key.key == ' '){
+        velocity-= updrift
+    }
+}
