84b0695bc6700a69d1dc68d7128d49f507c03641
mierjar <65968973+mierjar@users.noreply.github.com>
2022-04-21 15:38:00 +0200

    hey

---
+++ 01-node-init/index.js
@@ -0,0 +1,24 @@
+console.log('jeg er en node server')
+
+const express = require('express')
+const app = express()
+const mqtt = require('mqtt')
+const client  = mqtt.connect('wss://mqtt.nextservices.dk')
+
+client.on('connect', function () {
+  console.log('connected to next services')
+  console.log('subscribing to topic programmering')
+  client.subscribe('programmering')
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  //client.end()
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
